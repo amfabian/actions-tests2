83a57c95cc809903e1a4c178a4dfc3964e9a96b9
Alexandre Fabian <xandy.fabian@gmail.com>
2022-10-24 20:47:54 -0300

    Update GreetingResource.java

--- src/main/java/dev/ifrs/GreetingResource.java
+++ src/main/java/dev/ifrs/GreetingResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 // Comentario
+    // segundo comentario
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
